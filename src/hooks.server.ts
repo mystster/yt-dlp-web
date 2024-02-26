16b0c1327b97ff77282e3c5b901b08b899821f63
mystster <mystster@gmail.com>
2024-02-26 15:00:32 +0000

    refactor: define mqtt port in .env

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,19 +3,21 @@ import { router } from '$lib/trpc/router';
 import type { Handle } from '@sveltejs/kit';
 import { createTRPCHandle } from 'trpc-sveltekit';
 import portscanner from 'portscanner';
+import { PUBLIC_MQTT_WEBSOCKET_PORT } from '$env/static/public';
 
 import Aedes from 'aedes';
 import { createServer } from 'aedes-server-factory';
 
 export const handle: Handle = createTRPCHandle({ router, createContext });
+const port = parseInt(PUBLIC_MQTT_WEBSOCKET_PORT); 
 
-portscanner.checkPortStatus(1833, 'localhost', (err, status) => {
-	if (status == 'open') {
+portscanner.checkPortStatus( port, 'localhost', (err, status) => {
+	if (status == 'closed') {
 		const aedes = new Aedes();
 		const server = createServer(aedes, { ws: true });
 
-		server.listen(1883, function () {
-			console.log('mqtt broker with websocket started and listening on port ', 1883);
+		server.listen(port, function () {
+			console.log('mqtt broker with websocket started and listening on port ', port);
 		});
 	} else {
 		console.log('mqtt broker already running');
